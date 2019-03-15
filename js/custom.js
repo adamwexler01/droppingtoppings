a143d10501e9f1146c76834fa1d25d0b878877a7
DESKTOP-OTT9L4T\Damo <damosdesigns+vr@gmail.com>
2019-03-14 18:46:10 -0700

    semitransparent bg

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,3 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-47450936-5"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-47450936-5');
-</script>
-
-
 // Closes the sidebar menu
 $("#menu-close").click(function(e) {
   e.preventDefault();
